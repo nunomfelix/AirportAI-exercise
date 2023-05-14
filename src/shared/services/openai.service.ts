f768515c161e06faba60af3143ba5bfafcd9ade3
Nuno Félix <nunomfelix@ua.pt>
2023-05-14 13:09:02 +0100

    updated openai service to retry upon JSON.parse Error

--- src/shared/services/openai.service.ts
+++ src/shared/services/openai.service.ts
@@ -13,12 +13,38 @@ export class OpenAiService {
 
     const openai = new OpenAIApi(config);
 
-    const gptResponse = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `I need you to act as a context deriver. I will provide you with a sentence it will be your job to produce a json object of the following keys: type, brand and color based on details from the message. Don't print anything else but the JSON object. The message is '${message}'`,
-      max_tokens: 60,
-    });
+    let gptResponse;
+    let parsedResponse;
+    let hasError;
+    let retryCount = 0;
+    const maxRetries = 5; // specify the maximum number of retries
 
-    return JSON.parse(gptResponse.data.choices[0].text.trim());
+    do {
+      hasError = false;
+
+      try {
+        gptResponse = await openai.createCompletion({
+          model: 'text-davinci-003',
+          prompt: `I need you to act as a context deriver. I will provide you with a sentence it will be your job to produce a json object of the following keys: type, brand and color based on details from the message. Don't print anything else but the JSON object. The message is '${message}'`,
+          max_tokens: 60,
+        });
+
+        parsedResponse = JSON.parse(gptResponse.data.choices[0].text.trim());
+      } catch (error) {
+        hasError = true;
+        retryCount++;
+        console.error(
+          `Failed to parse JSON response. Retry count: ${retryCount}. Error: ${error.message}`,
+        );
+      }
+    } while (hasError && retryCount < maxRetries);
+
+    if (retryCount === maxRetries) {
+      throw new Error(
+        'Failed to parse JSON response after maximum number of retries',
+      );
+    }
+
+    return parsedResponse;
   }
 }
